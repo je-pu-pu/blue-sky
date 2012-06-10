ac443e6042abb08b1f36336498259ab79ecc2999
JE <je@je-pu-pu.jp>
2012-06-10 06:05:31 +0000

    古いソースファイルを削除。DirectX9 -> DirectX11 に移行中。
    git-svn-id: svn://localhost/sorato@178 30a34028-fe40-074e-a009-c9dacef5f2e4

--- branch/blue-sky-canvas-3d/DirectX.h
+++ branch/blue-sky-canvas-3d/DirectX.h
@@ -3,8 +3,8 @@
 
 #include <common/exception.h>
 
-#include <dxerr9.h>
-#pragma comment( lib, "dxerr9.lib" )
+#include <dxerr.h>
+#pragma comment( lib, "dxerr.lib" )
 
 #define DIRECT_X_RELEASE( x ) if ( x ) { x->Release(); x = 0; }
 #define DIRECT_X_FAIL_CHECK( x ) { HRESULT hresult = x; if ( FAILED( hresult ) ) { throw common::exception< HRESULT >( __FILE__, __LINE__, hresult ); } }
